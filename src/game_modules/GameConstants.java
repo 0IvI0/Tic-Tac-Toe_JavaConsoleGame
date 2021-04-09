763c886c7ae2e46d21d5cf35c04e22a7dc74f0bb
Ivett Kiss <iv.kiss@yahoo.com>
2021-04-09 12:41:15 +0200

    Added file with constant text messages for the gameplay

---
+++ src/game_modules/GameConstants.java
@@ -0,0 +1,13 @@
+package game_modules;
+
+class GameConstants {
+
+    static final String GAME_TITLE = "Tic Tac Toe";
+    static final char PLAYER_X = 'X';
+    static final char PLAYER_O = 'O';
+    static final String INPUT_COORDINATE = "Coordinates for ";
+    static final String INPUT_ERROR = "Incorrect input. Please try again.";
+    static final String GAME_DRAW = "The game has ended in a DRAW.";
+    static final String GAME_WINNER = "GAME OVER! The winner: ";
+
+}
